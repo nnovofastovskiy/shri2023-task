0075243c5baff0bfe9bc933233d36cae8233c820
Nikita <bananchik26@gmail.com>
2023-07-06 02:27:37 +0300

    Revert "css"
    This reverts commit c630c11c4f674e7a700a4ad121eb3b53c26c6ed9.

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -38,7 +38,11 @@ module.exports = {
     },
     optimization: {
         minimize: true,
-        minimizer: [new CssMinimizerPlugin()],
+        minimizer: [
+            // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+            `...`,
+            new CssMinimizerPlugin(),
+        ],
     },
     plugins: [new MiniCssExtractPlugin()],
 };
