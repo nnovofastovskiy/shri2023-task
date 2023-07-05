c630c11c4f674e7a700a4ad121eb3b53c26c6ed9
Nikita <bananchik26@gmail.com>
2023-07-06 02:09:16 +0300

    css

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -38,11 +38,7 @@ module.exports = {
     },
     optimization: {
         minimize: true,
-        minimizer: [
-            // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-            `...`,
-            new CssMinimizerPlugin(),
-        ],
+        minimizer: [new CssMinimizerPlugin()],
     },
     plugins: [new MiniCssExtractPlugin()],
 };
