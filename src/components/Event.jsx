8b00e8580e4c6bd065b93724dd9399735aafab1c
Nikita <bananchik26@gmail.com>
2023-07-06 16:30:49 +0300

    build ok

---
+++ src/components/Event.jsx
@@ -0,0 +1,23 @@
+import { useRef, useEffect } from "react";
+
+export function Event({ onSize, slim, icon, iconLabel, title, subtitle }) {
+    const ref = useRef();
+
+    useEffect(() => {
+        const width = ref.current.offsetWidth;
+        const height = ref.current.offsetHeight;
+        if (onSize) {
+            onSize({ width, height });
+        }
+    });
+
+    return <li ref={ref} className={'event' + (slim ? ' event_slim' : '')}>
+        <button className="event__button">
+            <span className={`event__icon event__icon_${icon}`} role="img" aria-label={iconLabel}></span>
+            <h4 className="event__title">{title}</h4>
+            {subtitle &&
+                <span className="event__subtitle">{subtitle}</span>
+            }
+        </button>
+    </li>;
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Header } from './components/Header';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>
 );
